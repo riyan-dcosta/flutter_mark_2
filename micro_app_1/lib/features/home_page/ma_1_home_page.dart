d1909716af38eac11a377aa96dc073286d2b5b52
riyan-dcosta <rdriyan@gmail.com>
2023-08-08 14:31:20 +0530

    micro app 1 initial

--- micro_app_1/lib/features/home_page/ma_1_home_page.dart
+++ micro_app_1/lib/features/home_page/ma_1_home_page.dart
@@ -1,11 +1,8 @@
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MA1MyApp extends StatelessWidget {
+  const MA1MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
