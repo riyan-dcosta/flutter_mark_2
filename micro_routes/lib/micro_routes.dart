a3bc66089a0a40c58369117e57fd304fc1ec527b
riyan-dcosta <rdriyan@gmail.com>
2023-08-08 14:30:07 +0530

    micro_routes with 1 micro app

--- micro_routes/lib/micro_routes.dart
+++ micro_routes/lib/micro_routes.dart
@@ -1,7 +1,3 @@
-library micro_routes;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'routes/application1_routes.dart';
+export 'routes/application2_routes.dart';
